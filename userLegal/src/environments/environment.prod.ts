ca908e3d96bd66b2434b020af1098d339fc7366d
Ezequiel <ezegonzalez800@gmail.com>
2024-06-14 14:11:15 -0300

    enviromnt

--- userLegal/src/environments/environment.prod.ts
+++ userLegal/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCFQ1v-2lDvQyCidenRGnREeLyml5s84a8",
-    authDomain: "pdf-manager-7fbf8.firebaseapp.com",
-    projectId: "pdf-manager-7fbf8",
-    storageBucket: "pdf-manager-7fbf8.appspot.com",
-    messagingSenderId: "775859610688",
-    appId: "1:775859610688:web:af2da5023833562c5eeba5",
-    measurementId: "G-DVHFJGDYQ1"
+   apiKey: "AIzaSyDAKIRLIGQYjBEjxuFp3Sr3uS8CGS3ldHg",
+  authDomain: "expotools-47221.firebaseapp.com",
+  databaseURL: "https://expotools-47221-default-rtdb.firebaseio.com",
+  projectId: "expotools-47221",
+  storageBucket: "expotools-47221.appspot.com",
+  messagingSenderId: "387954696866",
+  appId: "1:387954696866:web:ffc5e8e29a1df6bf8cedfb",
+  measurementId: "G-C5FHVXMXD7"
   }
 };
--- userLegal/src/environments/environment.ts
+++ userLegal/src/environments/environment.ts
@@ -6,15 +6,26 @@
 export const environment = {
   production: false,
   firebase: {
-   apiKey: "AIzaSyDAKIRLIGQYjBEjxuFp3Sr3uS8CGS3ldHg",
+  //  apiKey: "AIzaSyDAKIRLIGQYjBEjxuFp3Sr3uS8CGS3ldHg",
+  // authDomain: "expotools-47221.firebaseapp.com",
+  // projectId: "expotools-47221",
+  // storageBucket: "expotools-47221.appspot.com",
+  // messagingSenderId: "387954696866",
+  // appId: "1:387954696866:web:1e3b8ff25d9b53558cedfb",
+  // measurementId: "G-JQ6MNC3HR9"
+  apiKey: "AIzaSyDAKIRLIGQYjBEjxuFp3Sr3uS8CGS3ldHg",
   authDomain: "expotools-47221.firebaseapp.com",
+  databaseURL: "https://expotools-47221-default-rtdb.firebaseio.com",
   projectId: "expotools-47221",
   storageBucket: "expotools-47221.appspot.com",
   messagingSenderId: "387954696866",
-  appId: "1:387954696866:web:1e3b8ff25d9b53558cedfb",
-  measurementId: "G-JQ6MNC3HR9"
+  appId: "1:387954696866:web:ffc5e8e29a1df6bf8cedfb",
+  measurementId: "G-C5FHVXMXD7"
   },
 };
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
